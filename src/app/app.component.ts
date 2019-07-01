c6e0a641458d3a06933c47f4fda58a230990203f
eckline-work <ekline@thedoctors.com>
2019-07-01 15:07:38 -0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   public show: boolean = false;
+  public show2: boolean = false;
   public SIB: any = 'Show Server/Service Information';
 
   ngOnInit() { }
@@ -19,6 +20,10 @@ export class AppComponent  {
     else {
       this.SIB = 'Show Server/Service Information';
     }
+  }
+
+  toggle2() {
+    this.show2 = !this.show2;
   }
 }
 
