0f288b41733db4dc327fc1778a98ebb8ccf94c82
eckline-work <ekline@thedoctors.com>
2019-07-01 10:15:11 -0700

    3 files modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { ServiceWorkerModule } from '@angular/service-worker';
-
 
 import { AppComponent } from './app.component';
-import { SampleComponent} from './sample-component'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })],
+  imports:      [ BrowserModule, FormsModule, ],
   declarations: [ AppComponent ],
-  entryComponents:[ SampleComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
