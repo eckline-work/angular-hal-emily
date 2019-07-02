4faf48f3023d21f601ddc8999f5e4cbce6b45369
eckline-work <ekline@thedoctors.com>
2019-07-02 15:18:21 -0700

    3 files added

---
+++ src/app/date.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+/** @title Basic datepicker */
+@Component({
+  selector: 'datepicker-overview-example',
+  templateUrl: './datepicker.component.html',
+  styleUrls: ['./datepicker.component.css'],
+})
+export class DatepickerOverviewExample {}
