be8c928afa2587ea913ae329e3ad014f5e23c033
eckline-work <ekline@thedoctors.com>
2019-07-02 13:01:32 -0700

    3 files modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { DatepickerOverviewExample } from './datepicker-overview-example';
 import { AppComponent } from './app.component';
 
 @NgModule({
