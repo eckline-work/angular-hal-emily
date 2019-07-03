bd1b12c37ca3aa8f4406089b8bfb39f5f89c30ed
eckline-work <ekline@thedoctors.com>
2019-07-03 14:11:00 -0700

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [ AppComponent, ],
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, NgbModule ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
