f37061bffd4a3267f7e5dcf863ddfe443a7ae634
eckline-work <ekline@thedoctors.com>
2019-07-03 09:54:44 -0700

    3 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {MatDatepickerModule} from '@angular/material/datepicker'
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [ AppComponent, ],
-  imports:      [ BrowserModule, FormsModule, MatFormFieldModule, MatDatepickerModule ],
+  imports:      [ BrowserModule, FormsModule ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/date.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-/** @title Basic datepicker */
-@Component({
-  selector: 'datepicker-overview-example',
-  templateUrl: './datepicker.component.html',
-  styleUrls: ['./datepicker.component.css'],
-})
-export class DatepickerOverviewExample {}
