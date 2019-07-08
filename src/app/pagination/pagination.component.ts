f0627f8b72037faaaf55cc150c3b31a9f0c3f36f
eckline-work <ekline@thedoctors.com>
2019-07-08 14:55:43 -0700

    1 file modified

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -10,6 +10,10 @@ export class NgbdPagination {
   refTime = 30;
 
   critSet() {
+    //figuring out how to code this part is driving me insane.
+    //On click, should set values equal to selections, then use said values to parse through the table.
   }
+
+  //I don't even know how I'll get the message viewer to work...
 
 }
