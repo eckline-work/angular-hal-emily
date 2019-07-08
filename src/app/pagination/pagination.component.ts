084a51df3179ae5a45ec864245a757d0bc8fd104
eckline-work <ekline@thedoctors.com>
2019-07-08 15:10:36 -0700

    1 file modified

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -9,11 +9,16 @@ export class NgbdPagination {
   pageSize = 26;
   refTime = 30;
 
+
   critSet() {
     //figuring out how to code this part is driving me insane.
     //On click, should set values equal to selections, then use said values to parse through the table.
+    //At least I found a webpage I can use as a reference, I'll begin working through this tomorrow.
   }
 
-  //I don't even know how I'll get the message viewer to work...
+  //I don't even know how I'll get the message viewer to work.
+  //It will definitely need it's own set of functions.
+  //I'll code it after I finish the form function.
+  //Hopefully they won't take too long to do.
 
 }
