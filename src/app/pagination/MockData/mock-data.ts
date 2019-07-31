67c1f75520df0c19c6636a14e8fb5c5518c573b7
eckline-work <ekline@thedoctors.com>
2019-07-31 14:05:36 -0700

    4 files modified

--- src/app/pagination/MockData/mock-data.ts
+++ src/app/pagination/MockData/mock-data.ts
@@ -3,9 +3,9 @@ import {Inform} from './data';
 export const MockData: Inform[] = [ //What's your ****ing problem?!
 
   {sys: 'Data Warehouse',
-  msg: "Integer posuere erat a ante. Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh. Nullam id dolor id nibh ultricies vehicula ut id elit.",
-  prc: 'Vivamus porta sem malesuada magna mollis lacus vel sagittis augue laoreet dolor auctor. Something Integer',
-  trc: 'For sem malesuada magna, vehicula ut id elit.',
+  msg: "guess who needs to fix the table formatting again?",
+  prc: "Hint: It's me.",
+  trc: 'Screaming',
   env: 'TESTENV',
   CD: '2019-07-31',
   PD: '2019-07-31',
@@ -17,19 +17,41 @@ export const MockData: Inform[] = [ //What's your ****ing problem?!
     }
   },
 
-  {sys: 'Data Warehouse',
-  msg: "Integer posuere erat a ante. Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh. Nullam id dolor id nibh ultricies vehicula ut id elit.",
-  prc: 'Vivamus porta sem malesuada magna mollis lacus vel sagittis augue laoreet dolor auctor. Something Integer',
+  {sys: 'Star',
+  msg: "Boop.",
+  prc: 'Vivamus porta sem malesuada magna mort lacus vel sagittis augue laoreet dolor auctor. Something Integer',
+  trc: 'What is up, my good nerds?',
+  env: 'Dev',
+  CD: '2019-07-29',
+  PD: '2019-07-29',
+  ID: 777,
+  err: 'N'
+  },
+
+  {sys: 'Online Application',
+  msg: "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.",
+  prc: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+  trc: "asklajoif gjneircv hgpds hgviud jhsirea kehjrfiuehrs bgfidrghiug shfidrs buisf hfviuyrg hsgbuifa kalfg hid",
+  env: 'NPTES520',
+  CD: '2019-06-14',
+  PD: '2019-06-14',
+  ID: 711,
+  err: 'N',
+  eT: {
+    code: 'NOVA_NOTI',
+    desc: 'Nova Notification'
+    }
+  },
+
+  {sys: 'Claims Management',
+  msg: "Integer posuere erat a ante. Fusce dapibus, tellus ac cursus commod, tortor mauris nibh. Nullam id dolor id nibh ultricies vehicula ut id elit.",
+  prc: 'Vivamus porta sem malesuada magna mollis lacus vel sagittis augue laoreet dolor auctor. Something commodo',
   trc: 'For sem malesuada magna, vehicula ut id elit.',
   env: 'TESTENV',
   CD: '2019-07-26',
   PD: '2019-07-26',
   ID: 666,
-  err: 'Y',
-  eT: {
-    code: 'DB_ERROR',
-    desc: 'Database Error'
-    }
+  err: 'N'
   },
 
   {sys: 'Data Warehouse',
@@ -50,7 +72,7 @@ export const MockData: Inform[] = [ //What's your ****ing problem?!
   {sys: 'Nova Informatica',
   msg: "Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.",
   prc: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
-  trc: "asklajoif gjneircv hgpds hgviud jhsirea kehjrfiuehrs bgfidrghiug shfidrs buisf hfviuyrg hsgbuifa kalfg hid",
+  trc: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
   env: 'TEST_ENV',
   CD: '2019-07-25',
   PD: '2019-07-25',
@@ -63,7 +85,7 @@ export const MockData: Inform[] = [ //What's your ****ing problem?!
   },
 
   {sys: 'DBA',
-  msg: "Vivamus pellentesque ornare sem lacinia quam venenatis vestibulum. Nesciunt tofu stumptown aliqua,. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Cosby sweater eu tellus ac cursus commodo, tortor mauris nibh. This has been a long (not very) witty coment",
+  msg: "Vivamus pellentesque ornare sem lacinia quam venenatis vestibulum. Nesciunt tofu stumptown aliqua,. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Cosby sweater eu tellus ac cursus commodo, tortor mauris boop. This has been a long (not very) witty coment",
   prc: 'Vivamus lacus vel sagittis augue laoreet dolor auctor, tortor mauris nibh.',
   trc: 'For sem malesuada magna, vehicula ut id elit.',
   env: 'ENV2',
@@ -74,6 +96,33 @@ export const MockData: Inform[] = [ //What's your ****ing problem?!
   eT: {
     code: 'INFO',
     desc: 'Informational'
+    }
+  },
+
+  {sys: 'DBA',
+  msg: "Pellentesque ornare sem lacinia quam venenatis vestibulum. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Apparently the formatting looks wonky unless I make this one even longer, tortor mauris nibh. This has been a long (not very) witty coment",
+  prc: 'Vivamus porta duis mollis, quam vel sagittis augue laoreet dolor auctor.',
+  trc: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.',
+  env: 'ENV',
+  CD: '2019-07-21',
+  PD: '2019-07-21',
+  ID: 69,
+  err: 'N'
+  },
+
+  {sys: 'Nova',
+  msg: "Integer posuere erat a ante. Fusce dapibus, tellus ac cursus commodo, tortor mauris nibh. Nullam id dolor id nibh ultricies vehicula ut id elit.",
+  prc: 'Fusce porta sem malesuada magna mollis lacus vel sagittis augue laoreet dolor auctor. Something Integer',
+  trc: 'For sem malesuada magna, vehicula ut id elit.',
+  env: 'TESTENV',
+  CD: '2019-07-21',
+  PD: '2019-07-21',
+  ID: 34,
+  err: 'Y',
+  eT: {
+    code: 'NOVA_ERROR',
+    desc: 'Nova Errors',
+    email: 'name@email.com'
     }
   },
 
@@ -101,6 +150,21 @@ export const MockData: Inform[] = [ //What's your ****ing problem?!
     code: 'NOVA_ERROR',
     desc: 'Nova Errors',
     email: 'name@email.com'
+    }
+  },
+
+  {sys: 'Nova',
+  msg: "long witty coment to act as a placeholder so I can try to debug this stupid thing. Apparently the formatting looks wonky unless I make this one even longer, so more random typing it is. I don't actually know what to put here, this is just filler after all... Ok, this is long enough to make the formatting behave itself, so that's good. I should probably stop now. Ok, actually stoping in 3, 2, 1",
+  prc: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non quam venenatis vestibulum.',
+  trc: 'Commencing Keyboard smash in 5... 4...',
+  env: 'NPTOTHER',
+  CD: '2019-06-14',
+  PD: '2019-06-14',
+  ID: 13,
+  err: 'N',
+  eT: {
+    code: 'INFO',
+    desc: 'Informational'
     }
   },
 
--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -83,8 +83,8 @@ export class NgbdPagination {
 
   //scroll Code
   array = [];
-  sum = 20;
-  throttle = 60;
+  sum = 12;
+  throttle = 36;
   scrollDistance = 1;
 
   onScroll() {
