452cc096d121a8f19804c050ee89ce6e7e82ba0e
eckline-work <ekline@thedoctors.com>
2019-08-02 14:49:58 -0700

    2 files added

---
+++ src/app/pagination/messageViewer/messageView.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+import {Inform} from '../MockData/data';
+import {MockData} from '../MockData/mock-data';
+import {errorType} from '../MockData/type';
+
+@Component({
+  selector: 'messageViewer',
+  templateUrl: './messageView.component.html',
+  styleUrls: [ '../pagination.component.css' ]
+})
+
+export class messageViewer {
+  
+}
