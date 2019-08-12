582c1ddbbbed90deb4b11ed51e009039fb5bfb75
eckline-work <ekline@thedoctors.com>
2019-08-12 09:46:03 -0700

    1 file modified

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
 import { Inform } from '../pagination/MockData/data';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
