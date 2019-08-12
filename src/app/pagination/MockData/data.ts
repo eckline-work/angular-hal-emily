584db99d9d36f9e34e1cc1732ae5e1683f9a7212
eckline-work <ekline@thedoctors.com>
2019-08-12 14:22:34 -0700

    1 file modified

--- src/app/pagination/MockData/data.ts
+++ src/app/pagination/MockData/data.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Adapter } from '../../config/adapter'; //yes it is you soggy walnut
+import { Adapter } from '../../config/adapter'; //because it's an interface object, you soggy half-brained walnut
 import { errorType } from './type';
 
 export class Inform {
