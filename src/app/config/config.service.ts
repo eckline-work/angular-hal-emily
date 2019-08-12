0b14df6d936daea6d51b81abeeed3225e8748651
eckline-work <ekline@thedoctors.com>
2019-08-12 10:26:20 -0700

    1 file modified

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Inform } from '../pagination/MockData/data';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -9,13 +9,28 @@ import { map } from 'rxjs/operators';
 })
 export class ConfigService {
 
-  private baseUrl = '';
+  private baseUrl = 'http://api.myapp.com/data';
 
   constructor(private http: HttpClient) { }
 
   list(): Observable<Inform[]> {
     const url = `${this.baseUrl}/`;
-    return of([]);
+    return this.http.get(url).pipe(
+      map((data: any[]) => data.map((item: any) => new Inform(
+        item.SourceSystem,
+        item.LogNumber,
+        item.Message,
+        item.ProcessName,
+        item.TraceInfo,
+        item.EnvironmentInfo,
+        item.StartDate,
+        item.EndDate,
+        item.ID,
+        item.err,
+        item.eT,
+        item.UserName
+      ))),
+    );
   }
 
 }
